8c72e49f0d1b8f2751698d1e2ee94cf23ae5bdd8
Felipe Ferreira de Souza <S.FerreiraFelipe@live.com>
2020-10-17 17:26:13 -0300

    Criação e Configuração de Ambiente, Criação dos Projetos Dominio e DAL

---
+++ Nextoo.MeuPlano.Dominio/Planos/Enumerable/TipoDePlano.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wooza.MeuPlano.Dominio.Planos.Enumerable
+{
+    public enum TipoDePlano
+    {
+        Controle = 1,
+        Pós = 2,
+        Pré = 3
+    }
+}
---
+++ Nextoo.MeuPlano.Dominio/Planos/Plano.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wooza.MeuPlano.Dominio.Planos.Enumerable;
+
+namespace Wooza.MeuPlano.Dominio.Planos
+{
+    public class Plano
+    {
+        public int CodigoPlano { get; set; }
+
+        public int Minutos { get; set; }
+
+        public int FranquiaDeInternet { get; set; }
+
+        public int Valor { get; set; }
+
+        public TipoDePlano Tipo { get; set; }
+
+        public string Operadora { get; set; }
+    }
+}
