020a19125f021e817af2c1fb6dc03c9f560dffde
Felipe Ferreira de Souza <S.FerreiraFelipe@live.com>
2020-10-19 17:59:31 -0300

    Padrão Repository

---
+++ Nextoo.MeuPlano.DAL/Repository/Planos/IRepository/IPlanoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Nextoo.MeuPlano.DAL.Commom.Interface;
+using Nextoo.MeuPlano.Dominio.Planos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nextoo.MeuPlano.DAL.Repository.Planos.IRepository
+{
+    public interface IPlanoRepository : IRepository<Plano>
+    {
+
+    }
+}
---
+++ Nextoo.MeuPlano.DAL/Repository/Planos/PlanoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Nextoo.MeuPlano.DAL.Commom;
+using Nextoo.MeuPlano.DAL.Connection;
+using Nextoo.MeuPlano.DAL.Repository.Planos.IRepository;
+using Nextoo.MeuPlano.Dominio.Planos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nextoo.MeuPlano.DAL.Repository.Planos
+{
+    public class PlanoRepository : Repository<Plano>, IPlanoRepository
+    {
+        public PlanoRepository(ConnectionContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
