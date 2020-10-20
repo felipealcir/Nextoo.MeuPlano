74492b43eb50383f7e8ab7ac500e9f252e9067db
Felipe Ferreira de Souza <S.FerreiraFelipe@live.com>
2020-10-19 22:05:49 -0300

    Correção do .NET Core

---
+++ Nextoo.MeuPlano.API/Controllers/Planos/PlanoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Nextoo.MeuPlano.API.Controllers.Planos
+{
+    [Route("api/planos/v1")]
+    public class PlanoController
+    {
+
+    }
+}
--- Nextoo.MeuPlano.API/Startup.cs
+++ Nextoo.MeuPlano.API/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,26 +16,21 @@ namespace Nextoo.MeuPlano.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddOptions();
+            services.AddMvc(options =>
+                {
+                    options.OutputFormatters.Remove(new XmlDataContractSerializerOutputFormatter());
+                    options.EnableEndpointRouting = false;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseMvc();
         }
     }
 }
